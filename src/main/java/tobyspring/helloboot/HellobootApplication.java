204560d83408d4f3f38593139a137bb748bb7c95
chinchin <skykim10908@naver.com>
2023-01-29 18:54:53 +0900

    toby spring boot lecture - 스프링 부트 3.0 으로 변경 처리!

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import tobyspring.config.MySpringBootApplication;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 // @SpringBootApplication
 @MySpringBootApplication
